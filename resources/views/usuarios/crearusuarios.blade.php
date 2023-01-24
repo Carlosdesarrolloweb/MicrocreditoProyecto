343ca9ff67c732a6d609d00be786e23acfb7a33a
Carlosdesarrolloweb <carlitotuning295@gmail.com>
2023-01-24 00:41:43 -0300

    ultima act 12:41 pm

--- resources/views/usuarios/crearusuarios.blade.php
+++ resources/views/usuarios/crearusuarios.blade.php
@@ -15,63 +15,68 @@
         </x-slot>
 
         <x-jet-validation-errors class="mb-4" />
-
+        
         <form method="post" action="{{ route('user.create') }}">
             @csrf
-            <div>
+            <div>  
                 <x-jet-label for="Carnet" value="{{ __('Carnet/Dni') }}" />
-                <x-jet-input  maxlength="15" id="Carnet_usuario" class="block mt-1 w-full" type="text" name="Carnet_usuario" :value="old('Carnet_usuario')" required autofocus autocomplete="Carnet_usuario" />
+                <x-jet-input  maxlength="15" id="Carnet_usuario" class="block mt-1 w-full" style="WIDTH: 420px;"  type="text" name="Carnet_usuario" :value="old('Carnet_usuario')" required autofocus autocomplete="Carnet_usuario" />
             </div>
 
-            <div>
+            <div>  
                 <x-jet-label for="name" value="{{ __('Nombres') }}" />
-                <x-jet-input maxlength="20" id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-input maxlength="20" id="name" class="block mt-1 w-full" type="text" style="WIDTH: 420px;"  name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
-            <div>
+            <div>  
                 <x-jet-label for="Apellidos" value="{{ __('Apellidos') }}" />
-                <x-jet-input maxlength="20" id="apellido_usuario" class="block mt-1 w-full" type="text" name="apellido_usuario" :value="old('apellido_usuario')" required autofocus autocomplete="apellido_usuario" />
+                <x-jet-input maxlength="20" id="apellido_usuario" class="block mt-1 w-full" style="WIDTH: 420px;" type="text" name="apellido_usuario" :value="old('apellido_usuario')" required autofocus autocomplete="apellido_usuario" />
             </div>
-            <div>
+           
+            <div>  
                 <x-jet-label for="NombreUsuario" value="{{ __('Nombre de Usuario') }}" />
-                <x-jet-input maxlength="20"  id="Nombre_usuario" class="block mt-1 w-full" type="text" name="Nombre_usuario" :value="old('Nombre_usuario')" required autofocus autocomplete="Nombre_usuario" />
+                <x-jet-input maxlength="20"  id="Nombre_usuario" class="block mt-1 w-full" style="WIDTH: 420px"  type="text" name="Nombre_usuario" :value="old('Nombre_usuario')" required autofocus autocomplete="Nombre_usuario" />
             </div>
             <!--<div>
                 <x-jet-label for="Cargo" value="{{ __('Cargo Laboral') }}" />
                <x-jet-input maxlength="15" id="cargo_usuario" class="block mt-1 w-full" type="text" name="cargo_usuario" :value="old('cargo_usuario')" required autofocus autocomplete="cargo_usuario" />
-            </div> -->
+            </div> -->         
+            <div>  
             <x-jet-label for="Cargo" value="{{ __('Cargo Laboral') }}" />
                 <select name="cargo_usuario">
                 <option value="ADMINISTRADOR">ADMINISTRADOR</option>
                 <option value="ENCARGADO">ENCARGADO</option>
                 </select>
-            <div>
+            <div>  
                 <x-jet-label for="Direccion" value="{{ __('Direccion') }}" />
-                <x-jet-input maxlength="40" id="direccion_usuario" class="block mt-1 w-full" type="text" name="direccion_usuario" :value="old('direccion_usuario')" required autofocus autocomplete="direccion_usuario" />
+                <x-jet-input maxlength="40" id="direccion_usuario"  style="WIDTH: 420px; HEIGHT: 98px; HEIGHT: 98px" class="block mt-1 w-full" type="text" name="direccion_usuario" :value="old('direccion_usuario')" required autofocus autocomplete="direccion_usuario" />
             </div>
-            <div>
+            <div>  
                 <x-jet-label for="Telefono" value="{{ __('Telefono') }}" />
-                <x-jet-input maxlength="8" id="telefono_usuario" class="block mt-1 w-full" type="text" name="telefono_usuario" :value="old('telefono_usuario')" required autofocus autocomplete="telefono_usuario" />
+                <x-jet-input maxlength="8" id="telefono_usuario" style="WIDTH: 420px;" class="block mt-1 w-full" type="text" name="telefono_usuario" :value="old('telefono_usuario')" required autofocus autocomplete="telefono_usuario" />
             </div>
-            <div>
+            <div>  
                 <x-jet-label for="email" value="{{ __('Correo Electronico') }}" />
-                <x-jet-input maxlength="40" id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input maxlength="40" id="email" class="block mt-1 w-full"  style="WIDTH: 420px;" type="email"  name="email" :value="old('email')" required />
             </div>
-
-            <div>
+            <div>        
                 <x-jet-label for="password" value="{{ __('Contraseña') }}" />
-                <x-jet-input  id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-input  id="password" style="WIDTH: 420px;" class="block mt-1 w-full"  style="WIDTH: 420px;"  type="password" name="password" required autocomplete="new-password" />
             </div>
 
-            <div >
+          
+            <div>  
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input  id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-input  id="password_confirmation" class="block mt-1 w-full" style="WIDTH: 420px;"  type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
+            
             <div class="flex items-center justify-end mt-4">          
 
                 <x-jet-button class="ml-4">
                     {{ __('Registrar') }}
                 </x-jet-button>
             </div>
+       
+            
         </form>
     </x-jet-authentication-card>
 </x-guest-layout>
