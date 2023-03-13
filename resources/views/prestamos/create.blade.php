f15648d15b0adcb6f457dce00e0b7cc951846da2
Carlosdesarrolloweb <carlitotuning295@gmail.com>
2023-03-12 22:18:18 -0300

    ultimaact 2218 12/03/2023

--- resources/views/prestamos/create.blade.php
+++ resources/views/prestamos/create.blade.php
@@ -1,4 +1,6 @@
 
+
+
 @extends('adminlte::page')
 
 @section('title', 'Nuevo Préstamo')
@@ -17,34 +19,7 @@
                 <div class="box-body">
                     <form action="{{ route('prestamos.store') }}" method="POST">
                         @csrf
-                        <div class="form-group">
-                            <label for="monto_prestamo">Monto del Préstamo</label>
-                            <input type="number" id="monto"name="monto_prestamo" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="duracion_prestamo">Duración del Préstamo (en meses)</label>
-                            <input type="number" id="duracion" name="duracion_prestamo" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="calculo_cuota">Cálculo de Cuota</label>
-                            <input type="number" id="calculo_cuota" name="calculo_cuota" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="garantia">Garantía</label>
-                            <input type="text" id="garantia" name="garantia" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="cantidad_cuotas">Cantidad de Cuotas</label>
-                            <input type="number" id="cantidad_cuotas" name="cantidad_cuotas" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="monto_cancelado">Monto Cancelado</label>
-                            <input type="number"  id="monto_cancelar"name="monto_cancelado" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="monto_prestado">Monto Prestado</label>
-                            <input type="number" id="monto_prestado" name="monto_prestado" class="form-control" required>
-                        </div>
+
                         <div class="form-group">
                             <label for="id_cliente">Cliente</label>
                             <select name="id_cliente" class="form-control" required>
@@ -65,7 +40,7 @@
                         </div>
                         <div class="form-group">
                             <label for="id_interes">Tipo de Interés</label>
-                            <select name="id_interes" class="form-control" required>
+                            <select name="id_interes" id="interes" class="form-control" required>
                                 <option value="">Seleccione un tipo de interés</option>
                                 @foreach($intereses as $interes)
                                     <option value="{{ $interes->id }}">{{ $interes->interes_prestamo }}</option>
@@ -81,6 +56,39 @@
                                 @endforeach
                             </select>
                         </div>
+                        <div class="form-group">
+                            <label for="monto_prestamo">Monto del Préstamo</label>
+                            <input type="number" id="monto"name="monto_prestamo" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="duracion_prestamo">Duración del Préstamo (en meses)</label>
+                            <input type="number" id="duracion" name="duracion_prestamo" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="cantidad_cuotas">Cantidad de Cuotas</label>
+                            <input type="number" id="cantidad_cuotas" name="cantidad_cuotas" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="calculo_cuota">Cálculo de Cuota</label>
+                            <input type="number" id="calculo_cuota" name="calculo_cuota" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="monto_prestado">Monto Prestado</label>
+                            <input type="number" id="monto_prestado" name="monto_prestado" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="monto_cancelado">Monto Cancelado</label>
+                            <input type="number"  id="monto_cancelar"name="monto_cancelado" class="form-control" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="garantia">Garantía</label>
+                            <input type="text" id="garantia" name="garantia" class="form-control" required>
+                        </div>
+
+
                     </div>
     @stop
 
@@ -90,9 +98,30 @@
 
 @section('js')
 <script>
+    // Seleccionamos el input
+    const input1 = document.getElementById("interes");
+    const input2 = document.getElementById("id_modo_pago");
+    const input3 = document.getElementById("monto");
+    const input4 = document.getElementById("duracion");
+    const input5 = document.getElementById("cantidad_cuotas");
+    const input5 = document.getElementById("monto_cancelar");
 
+    // Agregamos un listener para el evento input
+    input1.addEventListener("input", () => {
+        if(input1.value.length>0){
 
-</script>
+            if(input2.value.length>0){
+
+                if(imput3.value.length>0 && input4.value.length>0)
+                var division=Number(input1.value)/10;
+                var montofinal=Number(input1.value)+division;
+
+            }
+
+        }
+      // Aquí puedes agregar la lógica que necesites para detectar el input
+    });
+  </script>
 
 
 
