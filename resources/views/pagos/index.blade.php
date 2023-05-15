576f9f9e0e07cd1663d158cfef86126ee2e91f7b
Carlosdesarrolloweb <carlitotuning295@gmail.com>
2023-05-15 00:02:15 -0400

    ULTIMA ACT 15/05/2023 0:02

--- resources/views/pagos/index.blade.php
+++ resources/views/pagos/index.blade.php
@@ -13,56 +13,20 @@
     </center>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
 @stop
-
-{{-- @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="bg-dark text-white">
-                                <tr>
-                                    <th>Cliente</th>
-                                    <th>Prestamo</th>
-                                    <th>Fecha de pago</th>
-                                    <th>Monto de pago</th>
-                                    <th>Descripción</th>
-                                    <th>Acciones</th> <!-- Agregamos una nueva columna para las acciones -->
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach ($pagos as $pago)
-                                    <tr>
-                                        <td>{{ $pago->prestamo->cliente->nombre_cliente}} {{ $pago->prestamo->cliente->apellido_cliente }}</td>
-                                        <td>{{ $pago->prestamo->monto_prestamo }}</td>
-                                        <td>{{ $pago->fecha_pago }}</td>
-                                        <td>{{ $pago->monto_pago }}</td>
-                                        <td>{{ $pago->descripcion }}</td>
-                                        <td>
-                                            <a href="{{ route('pagos.edit', $pago->id) }}" class="btn btn-primary btn-sm">Editar</a> <!-- Agregamos el botón Editar que redirige a la vista de edición -->
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@stop --}}
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
+<div class="container-fluid">
+    <x-slot name="header">
+    </x-slot>
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="example" class="table table-striped table-bordered" style="width:100%">
                                 <thead class="bg-dark text-white">
                                 <tr>
                                     <th>Cliente</th>
@@ -84,7 +48,7 @@
                                         <td>{{ $pago->prestamo->monto_prestado - $pago->prestamo->pagos->sum('monto_pago') }}</td>
                                         <td>{{ $pago->descripcion }}</td>
                                         <td>
-                                            <a href="{{ route('pagos.edit', $pago->id) }}" class="btn btn-primary btn-sm">Editar</a> <!-- Agregamos el botón Editar que redirige a la vista de edición -->
+                                            <a href="{{ route('pagos.edit', $pago->id) }}" class="btn btn-warning btn-edit">Editar</a> <!-- Agregamos el botón Editar que redirige a la vista de edición -->
                                         </td>
                                     </tr>
                                 @endforeach
@@ -104,7 +68,13 @@
 @section('js')
     <script src="{{ asset('vendor/jquery-table2excel/dist/jquery.table2excel.min.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script>
-
-    </script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <Script>
+        $(document).ready(function () {
+        $('#example').DataTable();
+        });
+    </Script>
 @stop
--- resources/views/prestamos/index.blade.php
+++ resources/views/prestamos/index.blade.php
@@ -13,51 +13,56 @@
 </center>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
 @stop
 
 @section('content')
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
+<div class="container-fluid">
+    <x-slot name="header">
+    </x-slot>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-striped table-hover">
-                            <thead class="bg-dark text-white">
-                            <tr>
+                        <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                <thead class="bg-dark text-white">
+                                <tr>
 
-                                <th>Cliente</th>
-                                <th>Interés</th>
-                                {{-- <th>Modo de Pago</th> --}}
-                                <th>Monto del Préstamo</th>
-                                <th>Duración del Préstamo (en meses)</th>
-                                <th>Cantidad de Cuotas</th>
-                                <th>Cálculo de Cuota</th>
-                                <th>Ganancia</th>
-                                <th>Monto Prestado</th>
-                                <th>Monto Cancelado</th>
-                                <th>Fecha Prestamo</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($prestamos as $prestamo)
-                            <tr>
-                                <td>{{ $prestamo->cliente->nombre_cliente }} {{ $prestamo->cliente->apellido_cliente }}</td>
-                                <td>{{ $prestamo->interes->interes_prestamo }}%</td>
-                                {{-- <td>{{ $prestamo->modo_pago->modalidad_pago }}</td> --}}
-                                <td>Bs.{{ $prestamo->monto_prestamo }}</td>
-                                <td>{{ $prestamo->duracion_prestamo }}</td>
-                                <td>{{ $prestamo->cantidad_cuotas }}</td>
-                                <td>Bs.{{ $prestamo->calculo_cuota }}</td>
-                                <td>Bs.{{ $prestamo->ganancia }}</td>
-                                <td>Bs.{{ $prestamo->monto_prestado }}</td>
-                                <td>Bs.{{ $prestamo->monto_cancelado }}</td>
-                                <td>{{ $prestamo->fecha_prestamo }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                    <th>Cliente</th>
+                                    <th>Interés</th>
+                                    {{-- <th>Modo de Pago</th> --}}
+                                    <th>Monto del Préstamo</th>
+                                    <th>Duración del Préstamo (en meses)</th>
+                                    <th>Cantidad de Cuotas</th>
+                                    <th>Cálculo de Cuota</th>
+                                    <th>Ganancia</th>
+                                    <th>Monto Prestado</th>
+                                    <th>Monto Cancelado</th>
+                                    <th>Fecha Prestamo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($prestamos as $prestamo)
+                                <tr>
+                                    <td>{{ $prestamo->cliente->nombre_cliente }} {{ $prestamo->cliente->apellido_cliente }}</td>
+                                    <td>{{ $prestamo->interes->interes_prestamo }}%</td>
+                                    {{-- <td>{{ $prestamo->modo_pago->modalidad_pago }}</td> --}}
+                                    <td>Bs.{{ $prestamo->monto_prestamo }}</td>
+                                    <td>{{ $prestamo->duracion_prestamo }}</td>
+                                    <td>{{ $prestamo->cantidad_cuotas }}</td>
+                                    <td>Bs.{{ $prestamo->calculo_cuota }}</td>
+                                    <td>Bs.{{ $prestamo->ganancia }}</td>
+                                    <td>Bs.{{ $prestamo->monto_prestado }}</td>
+                                    <td>Bs.{{ $prestamo->monto_cancelado }}</td>
+                                    <td>{{ $prestamo->fecha_prestamo }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -77,8 +82,12 @@
 @stop
 
 @section('js')
-<script src="{{ asset('vendor/jquery-table2excel/dist/jquery.table2excel.min.js') }}"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('vendor/jquery-table2excel/dist/jquery.table2excel.min.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script>
     $('th a').click(function(e){
         e.preventDefault();
@@ -96,4 +105,9 @@
         });
     });
 </script>
+<Script>
+    $(document).ready(function () {
+    $('#example').DataTable();
+    });
+</Script>
 @stop
