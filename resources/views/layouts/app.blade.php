39df0750509e2a256d328a5c19174f33983cd7ab
Eduardito187 <eduardchavez302@gmail.com>
2023-07-16 21:31:40 -0400

    correccion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
 
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
         @livewireStyles
         @powerGridStyles
     </head>
