845e3d0c63d7e014f1a8c9cb603cbf781092cf5d
Carlosdesarrolloweb <carlitotuning295@gmail.com>
2023-07-17 21:05:07 -0400

    ultimaact

--- resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,10 +1,19 @@
 
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white-100">
+{{-- <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white-100">
     <div>
     </div>
 
     <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
     <img src="{{URL::asset('https://files.fm/thumb_show.php?i=rh59me2xy')}}">
+        {{ $slot }}
+    </div>
+</div> --}}
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white-100">
+    <div>
+    </div>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <img src="{{ asset('logomicrocreditosmary.png') }}" alt="Logo Microcréditos Mary">
         {{ $slot }}
     </div>
 </div>
